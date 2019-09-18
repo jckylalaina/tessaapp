69a82aa71d2ba89a556933f3d42e5eae39ec69b7
jckylalaina <46310926+jckylalaina@users.noreply.github.com>
2019-09-18 13:45:16 +0300

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,7 +18,18 @@ Rails.start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+window.onscroll = function() {myFunction()};
 
+function myFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    document.getElementById("ftco-navbar").classList.remove('bg-transparent')
+    document.getElementById("ftco-navbar").classList.add('bg-dark')
+
+  } else {
+    document.getElementById("ftco-navbar").classList.add('bg-transparent')
+    document.getElementById("ftco-navbar").classList.remove('bg-dark')
+  }
+}
 
 $(document).on('turbolinks:load',function(){
 
